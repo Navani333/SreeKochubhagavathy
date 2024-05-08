cc05870098d40ff26b909ba166ee2e27505a6c15
ayanaanand <ayanaanand41@gmail.com>
2024-05-08 16:56:03 +0530

    ayana,office barreir 08/05/2024

---
+++ resources/views/office_bearers.blade.php
@@ -0,0 +1,448 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>sreekochubhagavathytemple-Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vlava
+  * Template URL: https://bootstrapmade.com/vlava-free-bootstrap-one-page-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+      <h1><a href="index.html">ശ്രീ കൊച്ചുഭഗവതി ക്ഷേത്രം</a></h1>
+        
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrolltoactive" href="{{ route('home') }}">Home</a></li>
+          <li><a class="nav-link scrollto" href="{{ route('history') }}">History</a></li>
+          <li><a class="nav-link scrollto" href="{{ route('festivals') }}">Festivals</a></li>
+          <li><a class="nav-link scrollto " href="{{ route('upadevadas') }}">Upadevadas</a></li>
+          <li><a class="nav-link scrollto " href="{{ route('office_bearers') }}">Office Bearers</a></li>
+          <li><a class="nav-link scrollto" href="{{ route('gallery') }}">Gallery</a></li>
+          
+          <li><a class="nav-link scrollto" href="{{ route('contact') }}">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <div class="slideshow-container">
+  <div class="mySlides">
+    <section id="hero">
+      <div class="hero-container">
+        <h1>ശ്രീ കൊച്ചുഭഗവതി ക്ഷേത്രം</h1>
+        <a href="" class="btn-get-started scrollto">Ask something</a>
+      </div>
+    </section>
+  </div>
+  
+  
+  <!-- Add more slides with the same structure as above -->
+</div><!-- End Hero -->
+
+
+<section class="ftco-section testimony-section bg-light">
+     <center> <h2 class="mb-3">നമ്മെ വിട്ടുപിരിഞ്ഞവർ</h2></center>
+		<center><img src="assets/img/office barreiers/appupan.jpg" width="300" height="300">
+</center><center><br><br><b>ക്ഷേത്ര സമുച്ചയത്തിനായി ആസ്ഥാനഭൂമി <br>ട്രുസ്ടിനു വിട്ടുനൽകിയ   
+നാച്ചിവിളാകത്തു<br> ശ്രീ കേശവപ്പണിക്കർ  </b></center></img>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f1f1f1;
+  padding: 20px;
+  font-family: Arial;
+}
+
+/* Center website */
+.main {
+  max-width: 1000px;
+  margin: auto;
+}
+
+h1 {
+  font-size: 50px;
+  word-break: break-all;
+}
+
+.row {
+  margin: 8px -16px;
+}
+
+/* Add padding BETWEEN each column */
+.row,
+.row > .column {
+  padding: 8px;
+}
+
+/* Create four equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 25%;
+}
+
+/* Clear floats after rows */ 
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Content */
+.content {
+  background-color: white;
+  padding: 10px;
+}
+
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media screen and (max-width: 900px) {
+  .column {
+    width: 50%;
+  }
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
+}
+</style>
+
+<!-- MAIN (Center website) -->
+<div class="main">
+
+<div class="row">
+&emsp;
+  <div class="column">
+    <div class="content">
+      <center><img src="assets/img/office barreiers/Chellan.jpg" alt="Mountains" style="width:78%"></center>
+       <h6> <center><b><br>ശ്രീ  കെ ചെല്ലൻ</b> </center></h6>
+    <center>  <span class="position">മുൻ  പ്രസിഡന്റ് </span>
+</center>    </div>
+  </div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+  <div class="column">
+    <div class="content">
+    <center><img src="assets/img/office barreiers/13.jpg" alt="Lights" style="width:78%"></center>
+      <h6><center><b><br>ശ്രീ ആർ സോമൻ</b></center></h6>
+     <center> <span class="position">മുൻ  പ്രസിഡന്റ് </span>
+</center>    </div></div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+	<div class="column">
+    <div class="content">
+   <center> <img src="/assets/img/office barreiers/14.jpg" alt="Lights" style="width:78%"></center>
+      <h6><center><b>ശ്രീ  സുകുമാരൻ നായർ</b></center></h6>
+    <center> <span class="position">മുൻ  പ്രസിഡന്റ് </span>
+</center>    </div></div>
+                   
+              
+        </div>
+      </div>
+    </section>
+	
+                     <section class="ftco-section testimony-section bg-light">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center heading-section ftco-animate">
+          	<span class="subheading"><b>Office Bearers</b></span><br><br>
+            <h2 class="mb-3">ക്ഷേത്ര ഭാരവാഹികൾ</h2>
+          </div>
+        </div>
+		<center><img src="assets/img/office barreiers//krish.jpg" width="200" height="200"></center><center><b>ശ്രീ  കെ.കൃഷ്ണൻതമ്പി<br><b>(രക്ഷാധികാരി)</b></center></img>
+       <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f1f1f1;
+  padding: 30px;
+  font-family: Arial;
+}
+
+/* Center website */
+.main {
+  max-width: 1000px;
+  margin: auto;
+}
+
+h1 {
+  font-size: 12px;
+  word-break: break-all;
+}
+
+.row {
+  margin: 8px -20px;
+}
+
+/* Add padding BETWEEN each column */
+.row,
+.row > .column {
+  padding: 8px;
+}
+
+/* Create four equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 25%;
+}
+
+/* Clear floats after rows */ 
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Content */
+.content {
+  background-color: white;
+  padding: 20px;
+}
+
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media screen and (max-width: 900px) {
+  .column {
+    width: 50%;
+  }
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
+}
+</style>
+
+<!-- MAIN (Center website) -->
+<div class="main">
+
+<div class="row">
+&emsp;
+<div class="column">
+    <div class="content">
+     <center> <img src="assets/img/office barreiers/radhakrishnan.jpg" alt="Mountains" style="width:78%"></center>
+       <br><h1 class="name"><center><b>ശ്രീ എസ്.രാധാകൃഷ്ണൻ</b> <br>
+                    <b> പ്രസിഡന്റ്</b></center> </p>
+    </div>
+  </div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+  <div class="column">
+    <div class="content">
+     <center> <img src="assets/img/office barreiers/gopi.jpg" alt="Mountains" style="width:78%"></center>
+       <br><h1 class="name"><center><b>ശ്രീ ബി.ഗോപിനാഥൻ </b> <br>
+                    <b>വൈസ് പ്രസിഡന്റ്</b></center> </p>
+    </div>
+  </div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+  <div class="column">
+    <div class="content">
+   <center> <img src="assets/img/office barreiers/4.jpg" alt="Mountains" style="width:78%"></center>
+      <br> <h1 class="name"><center><b>ശ്രീ  പി.എസ്.മേഘവർണ്ണൻ</b><br>
+                   <b> ജനറൽ സെക്രട്ടറി </b></center> </p>
+    </div></div>
+	<div class="column">
+    <div class="content">
+    <center> <img src="assets/img/office barreiers/Ajayakumar.jpg" alt="Mountains" style="width:70%"></center>
+      <br> <h1 class="name"><center><b>ശ്രീ  പി.കെ.അജയകുമാർ  </b><br>
+                   <b> ട്രഷറർ</b></center> </p>
+     
+    </div></div>&emsp;&emsp;&emsp;&emsp;&emsp;
+         
+	  
+	  
+	  
+  <div class="column">
+    <div class="content">
+     <center> <img src="assets/img/office barreiers/8.jpg" alt="Mountains" style="width:78%"></center>
+       <br><h1 class="name"><center><b>ശ്രീ  വി.ശ്രീകണ്ഠൻനായർ </b> <br>
+                    <b>പൂജ  സെക്രട്ടറി</b></center> </p>
+    </div>
+  </div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+  <div class="column">
+    <div class="content">
+   <center> <img src="assets/img/office barreiers/pratheep.jpg" alt="Mountains" style="width:78%"></center>
+      <br> <h1 class="name"><center><b>ശ്രീ എസ്.പ്രതീപ്‌കുമാർ</b><br>
+                   <b> ഉത്സവകമ്മിറ്റി സെക്രട്ടറി </b></center> </p>
+   
+    </div></div>
+    </section>
+
+
+	
+	
+	
+
+                     <section class="ftco-section testimony-section bg-light">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center heading-section ftco-animate">
+          	<span class="subheading">Office Bearers</span><br><br>
+            <h2 class="mb-3">ട്രസ്റ്റ് എക്സിക്യൂട്ടീവ് കമ്മിറ്റി അംഗങ്ങൾ</h2>
+          </div>
+        </div>
+		<div class="main">
+
+<div class="row">
+&emsp;
+  <div class="column">
+    <div class="content">
+      <center><img src="assets/img/office barreiers/img097.jpg" alt="Mountains" style="width:78%"></center>
+       <h1 class="name"> <b><br><center>ശ്രീ  കെ.സുരേന്ദ്രപണിക്കർ</b> </center></h1>
+      
+    </div>
+  </div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+  <div class="column">
+    <div class="content">
+    <center><img src="assets/img/office barreiers/panikar.jpg" alt="Lights" style="width:78%"></center>
+      <h1 class="name"><b><br><center>ശ്രീ  പി.സുരേന്ദ്രൻ</center></b></h1>
+      
+    </div></div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+	<div class="column">
+    <div class="content">
+    <center><img src="assets/img/office barreiers/ravi.jpg" alt="Lights" style="width:68%"></center>
+      <h1 class="name"><b><br><center>ശ്രീ  ആർ.രവി</center></b></h1>
+     
+    </div></div>
+           <div class="row">
+&emsp;
+  <div class="column">
+    <div class="content">
+      <center><img src="assets/img/office barreiers/2.jpg" alt="Mountains" style="width:78%"></center>
+       <h1 class="name"><b><br><center>ശ്രീ  കെ.എസ്.പ്രദീപ്</b> </center></h1>
+      
+    </div>
+  </div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+  <div class="column">
+    <div class="content">
+    <center><img src="assets/img/office barreiers/jeyan.jpg" alt="Lights" style="width:78%"></center>
+      <h1 class="name"><b><br><center>ശ്രീ  ജെ.ജയൻ</center></b></h1>
+      
+    </div></div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+	<div class="column">
+    <div class="content">
+    <center><img src="assets/img/office barreiers/5.jpg" alt="Lights" style="width:78%"></center>
+      <h1 class="name"><b><br><center>ശ്രീ  ജി.മുരുകേശൻ</center></b></h1>
+     
+    </div></div>        
+             <div class="row">
+&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+  <div class="column">
+    <div class="content">
+      <center><img src="assets/img/office barreiers/3.jpg" alt="Mountains" style="width:78%"></center>
+       <h1 class="name"><b><br><center>ശ്രീ  എസ്.അനിൽകുമാർ</b> </center></h1>
+      
+    </div>
+  </div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+  <div class="column">
+    <div class="content">
+    <center><img src="assets/img/office barreiers/sakthi.jpg" alt="Lights" style="width:68%"></center>
+      <h1 class="name"><b><br><center>ശ്രീ  ഡി.എം.ശക്തിധരൻ</center></b></h1>
+      
+    </div></div> 
+        </div>
+      </div>
+    </section>
+
+             
+<br>
+
+<footer id="footer">
+
+<div class="footer-top">
+
+  <div class="container">
+
+    <div class="row  justify-content-center">
+      <div class="col-lg-6">
+        <h4>ശ്രീ കൊച്ചുഭഗവതി ക്ഷേത്രം</h4>
+        <p>ശ്രീകൊച്ചുഭഗവതിക്ഷേത്രം, തിരുവനന്തപുരം, പ്ളാമൂട്ടുക്കട, രജി.നമ്പർ:164/86</p>
+      </div>
+    </div>
+
+    
+
+    <div class="social-links">
+      <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+      <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+      <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+      <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+    </div>
+
+  </div>
+</div>
+
+<div class="container footer-bottom clearfix">
+  <div class="copyright">
+    &copy; Copyright <strong><span>ശ്രീ കൊച്ചുഭഗവതി ക്ഷേത്രം</span></strong>. All Rights Reserved
+  </div>
+  <div class="credits">
+    <!-- All the links in the footer should remain intact. -->
+    <!-- You can delete the links only if you purchased the pro version. -->
+    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/vlava-free-bootstrap-one-page-template/ -->
+    Designed by <a href="https://bootstrapmade.com/">Soon....</a>
+  </div>
+</div>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
+
